55243ecad598a7f0935ce43139c3610ab0564bde
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-04-21 17:05:48 +0800

    add footer component

--- resources/js/layouts/app/AppNavigationLayout.vue
+++ resources/js/layouts/app/AppNavigationLayout.vue
@@ -2,6 +2,7 @@
 import type { BreadcrumbItemType } from '@/types';
 import AppTopNavigation from '@/components/AppTopNavigation.vue';
 import AppHeader from '@/components/AppClientHeader.vue';
+import AppFooter from '@/components/AppClientFooter.vue';
 
 interface Props {
     breadcrumbs?: BreadcrumbItemType[];
@@ -18,5 +19,6 @@ withDefaults(defineProps<Props>(), {
         <AppTopNavigation />
         <AppHeader />
         <slot />
+        <AppFooter />
     </div>
 </template>
