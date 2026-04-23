0d8f1626bcd9f7dba0b395781d548862d4b1ef5f
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-04-23 09:42:09 +0800

    add component

---
+++ resources/js/components/ValidToForeignBanner.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+
+</script>
+
+
+<template>
+    <div class="bg-[var(--warning-custom)] text-[var(--primary-custom)] p-2 font-roboto uppercase italic w-fit text-sm">
+        <p>VALID FOR FOREIGN ONLY!</p>
+    </div>
+</template>
