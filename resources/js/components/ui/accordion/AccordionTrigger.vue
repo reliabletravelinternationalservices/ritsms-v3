4d6df5108f0cd9e245fa93b7c166b3f39deb7117
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-04-23 14:54:25 +0800

    update component

--- resources/js/components/ui/accordion/AccordionTrigger.vue
+++ resources/js/components/ui/accordion/AccordionTrigger.vue
@@ -20,7 +20,7 @@ const delegatedProps = reactiveOmit(props, "class")
       v-bind="delegatedProps"
       :class="
         cn(
-          'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:underline [&[data-state=open]>svg]:rotate-180',
+          'flex flex-1 items-center justify-between py-4 font-medium transition-all hover:text-[var(--tertiary-custom)] [&[data-state=open]>svg]:rotate-180',
           props.class,
         )
       "
