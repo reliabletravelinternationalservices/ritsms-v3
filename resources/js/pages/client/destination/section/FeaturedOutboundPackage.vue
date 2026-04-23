c39aa70539c7215d9b8d9b7532a7350dadfcde64
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-04-23 10:10:55 +0800

    add component

--- resources/js/pages/client/destination/section/FeaturedOutboundPackage.vue
+++ resources/js/pages/client/destination/section/FeaturedOutboundPackage.vue
@@ -13,38 +13,38 @@ interface DestubationItem {
 const destinations: DestubationItem[] = [
   { 
     id: 1, 
-    name: 'Boracay', 
+    name: 'Europe', 
     images: [
-        'https://www.travelandtourworld.com/wp-content/uploads/2025/07/Boracays-White-Beach.jpg',
-        'https://www.exploreshaw.com/wp-content/uploads/2019/04/DSCF9807.jpg',
-        'https://lh4.googleusercontent.com/YzSscGxPCL74oOAQTBFdjZor8jFPdn-XtVTuFwDJmKrTqsZxQ0DnNHFogWSBRquTv1bKkA_suLix8nw1qcTGJ_F4wvSIHK5E2780BMFSQ7Bml5NAWdMs-_3uF2wsEOO9KySm71dWLItbSVVPdQ',
+        'https://blog.dookinternational.com/images/post-media/ulQFe1712299016.jpg',
+        'https://i.ytimg.com/vi/BZLZyX3JdEM/maxresdefault.jpg',
+        'https://avkasholidays.wordpress.com/wp-content/uploads/2015/06/eiffel-tower-paris-france-01.jpg',
     ] 
   },
   { 
     id: 2, 
-    name: 'Palawan', 
+    name: 'Japan', 
     images: [
-        'https://philippinetourismusa.com/wp-content/uploads/2019/05/palawan-slider-min.jpg',
-        'https://thehappytrip.com/wp-content/uploads/2018/08/1024px-Puerto_Princesa_Subterranean_River_National_park_01.jpg',
-        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0d/cb/a6/63/re-discovering-my-fave.jpg?w=1200&h=700&s=1',
+        'https://vj-prod-website-cms.s3.ap-southeast-1.amazonaws.com/shutterstock601341215huge-1675509836757.jpg',
+        'https://ik.imagekit.io/tvlk/blog/2024/10/Screenshot-2024-10-08-at-15.40.25-1024x674.jpg?tr=q-70,c-at_max,w-1000,h-600',
+        'https://ik.imagekit.io/tvlk/blog/2024/04/shutterstock_2295299767.jpg?tr=q-70,c-at_max,w-1000,h-600',
     ] 
    },
   { 
     id: 3, 
-    name: 'Batangas', 
+    name: 'South Korea', 
     images: [
-        'https://thehappytrip.com/wp-content/uploads/2019/03/taal-3259871_960_720.jpg',
-        'https://www.agoda.com/wp-content/uploads/2024/06/Featured-Tagaytay-Taal-Volcano-1244x700.jpg',
-        'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0b/c3/1a/96/what-a-beautiful-sunny.jpg?w=500&h=-1&s=1',
+        'https://ik.imagekit.io/tvlk/blog/2025/01/Screenshot-2025-01-07-at-00.24.37.jpg?tr=q-70,c-at_max,w-1000,h-600',
+        'https://www.onthegotours.com/repository/Gyeongbokgung-palace-in-autumn-Seoul--South-Korea--On-The-Go-Tours-324891477495442.jpg',
+        'https://d3hne3c382ip58.cloudfront.net/files/uploads/bookmundi/resized/cmsfeatured/things-to-do-in-south-korea-1533196984-785X440.jpg',
     ] 
   },
   { 
     id: 4, 
-    name: 'Baguio', 
+    name: 'Vietnam', 
     images: [
-        'https://thehappytrip.com/wp-content/uploads/2015/02/21034288_10154874698388017_8994094350056208787_n.jpg',
-        'https://static.tripzilla.ph/media/92692/conversions/free-tourist-spots-baguio-w1024.webp',
-        'https://www.brittany.com.ph/wp-content/uploads/2023/05/Igorot-Stone-Kingdom.jpg',
+        'https://media.viacation.com/blogs/ce6721c4cbae4110ae0c7.jpg',
+        'https://static.toiimg.com/photo/110640920.cms',
+        'https://www.crazysexyfuntraveler.com/wp-content/uploads/2018/11/The-magical-Halong-Bay.jpg',
     ] 
   },
 ]
@@ -52,22 +52,21 @@ const destinations: DestubationItem[] = [
 </script>
 
 <template>
-    <section  class="relative flex overflow-hidden flex-col gap-4 items-center justify-center min-h-screen md:min-h-[700px] lg:min-h-[900px] py-12 px-4 md:px-6 bg-[var(--inbound-custom)]">
+    <section  class="relative flex overflow-hidden flex-col gap-4 items-center justify-center min-h-screen md:min-h-[700px] lg:min-h-[900px] py-12 px-4 md:px-6 bg-[var(--outbound-custom)]">
         <div class="max-w-5xl mx-auto relative z-10 flex flex-col gap-4 items-center justify-center py-16">
             <div class="w-full flex flex-col md:flex-row md:items-end justify-between gap-6">
                 <div class="w-full md:w-2/3 space-y-2">
                     <div class="flex items-end gap-1 text-[var(--tertiary-custom)]">
-                        <Icon icon="boxicons:plane-land-filled" width="28" height="28" class="md:w-7 md:h-7" />
-                        <h2 class="font-bold font-roboto text-lg md:text-xl">INBOUND DESTINATIONS</h2>
+                        <Icon icon="fa7-solid:plane-departure" width="28" height="28" class="md:w-7 md:h-7" />
+                        <h2 class="font-bold font-roboto text-lg md:text-xl">OUTBOUND DESTINATIONS</h2>
                     </div>
                     <div class="space-y-3">
                         <h1 class="text-2xl md:text-3xl lg:text-4xl text-[var(--primary-custom)] uppercase font-montserrat font-bold">
-                            EXPLORE LOCAL SPOT HERE IN THE PHILIPPINES
+                            EXPLORE INTERNATIONAL DESTINATIONS WITH US
                         </h1>
                         <p class="font-roboto text-sm md:text-base text-[var(--primary-custom)]">
-                            Crystal clear waters, majestic cliffs, and endless sunshine. Your ultimate Philippine adventure starts right here.
+                            Cross borders and create memories. Experience seamless international travel to the world's most sought-after hidden gems and vibrant cities.
                         </p>
-                        <ValidToForeignBanner />
                     </div>
                 </div>
 
