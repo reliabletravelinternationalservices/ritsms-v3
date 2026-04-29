49894bc2f073d7338f8c0a8993f848b3de8de95f
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-04-29 16:58:06 +0800

    add route

--- routes/client.php
+++ routes/client.php
@@ -3,8 +3,10 @@
 use App\Http\Controllers\Client\ContactPageController;
 use App\Http\Controllers\Client\DestinationPageController;
 use App\Http\Controllers\Client\InboundDestinationController;
+use App\Http\Controllers\Client\InboundPackageDetailController;
 use App\Http\Controllers\Client\LandingPageController;
 use App\Http\Controllers\Client\OutboundDestinationController;
+use App\Http\Controllers\Client\OutboundPackageDetailController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandingPageController::class, 'index'])->name('client.landing');
@@ -13,11 +15,13 @@ Route::get('/destination', [DestinationPageController::class, 'index'])->name('c
 // OUTBOUND DESTINATIONS
 Route::prefix('outbound')->group(function () {
     Route::get('/', [OutboundDestinationController::class, 'index'])->name('client.outbound');
+    Route::get('/{package}', [OutboundPackageDetailController::class, 'index'])->name('client.outbound.index');
 });
 
 // INBOUND DESTINATIONS
 Route::prefix('inbound')->group(function () {
     Route::get('/', [InboundDestinationController::class, 'index'])->name('client.inbound');
+    Route::get('/{package}', [InboundPackageDetailController::class, 'index'])->name('client.inbound.index');
 });
 
 Route::get('/contact', [ContactPageController::class, 'index'])->name('client.contact');
