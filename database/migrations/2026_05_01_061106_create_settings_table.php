d2dbf96845b62724b077e6424d8cf2806e9fd04e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-01 15:03:38 +0800

    add table

---
+++ database/migrations/2026_05_01_061106_create_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('key')->unique(); // e.g. usd_to_php_rate
+            $table->text('value')->nullable(); // store string/number/json
+            $table->string('type')->default('string'); // string, number, json
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
