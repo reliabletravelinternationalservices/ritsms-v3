143d604d9370bf1fa1895f0e0e404026b47351b9
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-01 15:01:33 +0800

    update function

--- app/Http/Controllers/Client/LandingPageController.php
+++ app/Http/Controllers/Client/LandingPageController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
-use App\Repository\Package\PackageGroupRepository;
-use Illuminate\Http\Request;
+use App\Repository\Package\PackageRepository;
 use Inertia\Inertia;
 
 class LandingPageController extends Controller
 {
-    public function __construct(protected PackageGroupRepository $packageGroupRepository){}
+    public function __construct(protected PackageRepository $packageRepository) {}
+
     public function index()
     {
-        $inbound = $this->packageGroupRepository->getFirstFeaturedInboundGroupPackages();
-        $outbound = $this->packageGroupRepository->getFirstFeaturedOutboundGroupPackages();
+
+        $inbound = $this->packageRepository->getFeaturedInboundPackages();
+        $outbound = $this->packageRepository->getFeaturedOutboundPackages();
 
         return Inertia::render('client/home/LandingPage', compact('inbound', 'outbound'));
     }
