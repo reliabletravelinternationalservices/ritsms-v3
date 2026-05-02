13f643f15c371e840eef44c4e13e3a26cdc7ca95
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-02 14:19:53 +0800

    add seeder

--- database/seeders/PackageScheduleSeeder.php
+++ database/seeders/PackageScheduleSeeder.php
@@ -2,16 +2,26 @@
 
 namespace Database\Seeders;
 
+use App\Helpers\Json\JsonHelper;
+use App\Repository\Package\PackageScheduleRepository;
+use App\Static\SeederPath;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class PackageScheduleSeeder extends Seeder
 {
+    public function __construct(
+        protected SeederPath $path,
+        protected JsonHelper $jsonHelper,
+        protected PackageScheduleRepository $repository
+
+    ) {}
     /**
      * Run the database seeds.
      */
     public function run(): void
     {
-        //
+        $schedules = $this->jsonHelper->convertToCollection($this->path::PACKAGE_SCHEDULES)->all();
+        $this->repository->createManyPackageSchedule($schedules);
     }
 }
