2e345370b8bcb02cb725322c3760cf16434bc898
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-05 09:35:35 +0800

    add repo

--- app/Http/Controllers/Client/Package/PackageDetailController.php
+++ app/Http/Controllers/Client/Package/PackageDetailController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers\Client\Package;
 
 use App\Http\Controllers\Controller;
 use App\Models\Package;
+use App\Repository\Package\PackageRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PackageDetailController extends Controller
 {
+    public function __construct(protected PackageRepository $repository){}
     public function index(Package $package)
     {
-        $package->load('schedules', 'images');
+        $package = $this->repository->getPackageDetails($package->id);
         return Inertia::render('client/package/PackageDetailPage', compact('package'));
     }
 }
