663d52ddffdc425ae69d1cb933e9183d30645edd
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-06 11:18:42 +0800

    rename comtroller/page

--- resources/js/pages/client/package/InboundPage.vue
+++ resources/js/pages/client/package/InboundPage.vue
@@ -3,12 +3,22 @@ import AppLayout from '@/layouts/ClientAppLayout.vue';
 import { Head } from '@inertiajs/vue3';
 import InboundTitleHeader from './section/InboundTitleHeader.vue';
 import InboundPackageDisplay from './section/InboundPackageDisplay.vue';
+import { PackageGroup } from '@/types/group-package';
+
+interface Props {
+    'groups': {
+        'featured' : PackageGroup[],
+        'normal' : PackageGroup[],
+    }
+}
+
+const props = defineProps<Props>();
 </script>
 
 <template>
     <Head title="Explore Local Spots Here in The Philippines" />
     <AppLayout>
         <InboundTitleHeader />
-        <InboundPackageDisplay />
+        <InboundPackageDisplay :featuredGroups="props.groups.featured" :normalGroups="props.groups.normal" />
     </AppLayout>     
 </template>
