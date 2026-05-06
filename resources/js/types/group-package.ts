6ede033900cf885a3edd7dac07418773729512c7
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-06 10:25:32 +0800

    add interface

---
+++ resources/js/types/group-package.ts
@@ -0,0 +1,16 @@
+import { Package } from "./package";
+
+export interface PackageGroup {
+    id: number;
+    title: string;
+    description: string | null;
+    include_as_outbound: boolean;
+    include_as_inbound: boolean;
+    is_featured: boolean;
+    
+    // Relationship
+    packages?: Package[];
+    
+    created_at: string;
+    updated_at: string;
+}
