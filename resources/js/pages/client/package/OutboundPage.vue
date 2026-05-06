163056b4406837b6136de30bc3c24674aa83ac73
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-06 10:25:24 +0800

    rename page

---
+++ resources/js/pages/client/package/OutboundPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/ClientAppLayout.vue';
+import { Head } from '@inertiajs/vue3';
+import OutboundTitleHeader from './section/OutboundTitleHeader.vue';
+import OutboundPackageDisplay from './section/OutboundPackageDisplay.vue';
+import { PackageGroup } from '@/types/package';
+
+
+interface Props {
+    'groups': {
+        'featured' : PackageGroup[],
+        'normal' : PackageGroup[],
+    }
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+    <Head title="Explore International Destinations With Us" />
+    <AppLayout>
+        <OutboundTitleHeader  />
+        <OutboundPackageDisplay :featuredGroups="props.groups.featured" :normalGroups="props.groups.normal"/>
+    </AppLayout>     
+</template>
