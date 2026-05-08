9b9fd9b3fb4ae64dd642f3a14d730b9aefe10e1e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-08 14:20:14 +0800

    create component

---
+++ resources/js/components/ExploreButton.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+interface Props {
+    title: string;
+    href: string;
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <div class="border-b border-[var(--outbound-custom)] hover:border-[var(--tertiary-custom)] text-[var(--outbound-custom)] hover:text-[var(--tertiary-custom)] duration-75">
+        <a :href="href" target="_blank">
+            <button type="button" class="flex items-center gap-2 font-roboto italic text-sm">
+                {{ title }}
+                <Icon icon="ic:baseline-arrow-right-alt" class="text-md md:text-2xl" />
+            </button>
+        </a>
+    </div>
+</template>
