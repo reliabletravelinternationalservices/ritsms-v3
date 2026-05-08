78568893c32df5dde17a7072b4a98e5a13f489a3
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-08 16:26:28 +0800

    update controller

--- app/Http/Controllers/Client/Package/PackageGroupController.php
+++ app/Http/Controllers/Client/Package/PackageGroupController.php
@@ -3,13 +3,20 @@
 namespace App\Http\Controllers\Client\Package;
 
 use App\Http\Controllers\Controller;
+use App\Repository\Package\PackageGroupRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class PackageGroupController extends Controller
 {
-    public function index()
+    public function __construct(protected PackageGroupRepository $repository){}
+    public function index(int $id)
     {
-        return Inertia::render('client/package/PackageGroupPage');
+        // If your URL is /outbound/something/else, segment(1) is "outbound"
+        $type = request()->segment(1); 
+        
+        $isInbound = $type === 'inbound' ? true : false;
+        $group = $this->repository->getPackageGroupByID($id);
+        return Inertia::render('client/package/group/PackageGroupPage', compact('group', 'isInbound'));
     }
 }
