3a1b71ab3e29759ae562cb7a3db2084bb1099aec
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-09 16:27:00 +0800

    add destination data

--- app/Http/Controllers/Client/DestinationPageController.php
+++ app/Http/Controllers/Client/DestinationPageController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
+use App\Repository\Destination\DestinationRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class DestinationPageController extends Controller
 {
+    public function __construct(private DestinationRepository $repository) {}
     public function index()
     {
-        return Inertia::render('client/destination/DestinationPage');
+        $destinations = $this->repository->getAllDestinations();
+        return Inertia::render('client/destination/DestinationPage', compact('destinations'));
     }
 }
