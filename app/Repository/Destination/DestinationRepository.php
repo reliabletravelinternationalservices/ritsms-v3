82a0bef2e045a37eb2a16a23a65dc2b698657926
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-09 16:28:05 +0800

    add function

--- app/Repository/Destination/DestinationRepository.php
+++ app/Repository/Destination/DestinationRepository.php
@@ -18,4 +18,16 @@ class DestinationRepository
     {
         return collect($destinations)->map(fn (array $item) => $this->createDestination($item));
     }
+
+
+    public function getDestinationByID(int $id)
+    {
+        // This executes 1 query for destination + 2 queries for relations
+        return Destination::with(['locations', 'image'])->findOrFail($id);
+    }
+
+    public function getAllDestinations()
+    {
+        return Destination::with(['locations', 'locations.image', 'image'])->get();
+    }
 }
