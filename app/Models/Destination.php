33140aa9a5b3a951d3d1828852210a62e47e5aa0
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-09 10:56:57 +0800

    add relationship

---
+++ app/Models/Destination.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Destination extends Model
+{
+    protected $table = 'destinations';
+    
+    protected $fillable = [
+        'title',
+        'description',
+        'country',
+    ];
+
+
+    //Relationship
+    public function image()
+    {
+        return $this->morphOne(Media::class, 'mediable');
+    }
+
+    public function locations()
+    {
+        return $this->hasMany(DestinationLocation::class, 'destination_id');
+    }
+}
