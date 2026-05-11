e971ad479f7c392835b610c21d61b0e5c406b86e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-11 14:15:08 +0800

    fix lint

--- resources/js/pages/client/about/section/AgencyVideo.vue
+++ resources/js/pages/client/about/section/AgencyVideo.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="w-full">
         <div class="max-w-5xl m-auto py-8 flex flex-col gap-6">
-            <iframe class="m-auto" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F781032031404993%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+            <iframe class="m-auto" src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2Freel%2F781032031404993%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
         </div>
     </section>
 </template>
