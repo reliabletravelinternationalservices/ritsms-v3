0e3e7186a09894390fcd89224b5320c6154b22ba
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-11 16:14:21 +0800

    add column

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['admin', 'agent'])->default('agent');
+            $table->string('phone')->nullable();
+            $table->string('display_name');
             $table->rememberToken();
             $table->timestamps();
         });
