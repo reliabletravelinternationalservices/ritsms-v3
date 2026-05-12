22e61a0ea367f6f842a352a16a42fefc820d578d
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-12 16:57:27 +0800

    use new media interface

--- resources/js/types/package.ts
+++ resources/js/types/package.ts
@@ -1,20 +1,5 @@
-export interface Media {
-    id: number;
-    mediable_id: number;
-    mediable_type: string;
-    file_name: string;
-    file_path: string;
-    url: string;
-    disk: string;
-    type: string;
-    mime_type: string;
-    size: number;
-    alt_text?: string;
-    order_number: number;
-    is_primary: boolean;
-    created_at: string;
-    updated_at: string;
-}
+import { Media } from "./media";
+
 
 export interface Package {
     id: number;
@@ -24,8 +9,8 @@ export interface Package {
     base_price: number;
     down_payment: number;
     duration: number;
-    selling_start_date: string;
-    selling_end_date?: string;
+    selling_start_date?: string;
+    selling_end_date: string;
     highlights_array?: string[];
     itineraries_array: Array<{
         day: number;
