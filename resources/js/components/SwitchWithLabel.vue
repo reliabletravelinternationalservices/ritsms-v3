f86cf4414fa32efdc6a259018d53c4b8905d933e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-13 15:02:43 +0800

    add component

---
+++ resources/js/components/SwitchWithLabel.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+
+interface Props {
+    label?: string
+    isChecked: boolean
+    onChange?: (value: boolean) => void
+}
+
+const props = defineProps<Props>()
+
+</script>
+
+<template>
+  <div class="flex items-center space-x-2">
+    <Switch id="airplane-mode"  :is-checked="props.isChecked" @change="props.onChange" />
+    <Label for="airplane-mode">{{ label }}</Label>
+  </div>
+</template>
