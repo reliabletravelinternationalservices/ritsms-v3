ba6102027da630979c2c3b493f63d6e6ea8d0056
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-13 15:02:32 +0800

    make season a string

--- database/migrations/2026_04_29_034116_create_packages_table.php
+++ database/migrations/2026_04_29_034116_create_packages_table.php
@@ -27,7 +27,7 @@ return new class extends Migration
             $table->text('exclusions');
             $table->text('notes')->nullable();
             $table->string('destination');
-            $table->enum('season', ['spring', 'winter', 'autumn', 'summer', 'all_season'])->default('all_season');
+            $table->string('season');
             $table->boolean('is_foreign_only')->default(false);
             $table->timestamps();
         });
