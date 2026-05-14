9b507dba08191442f8008ca366dc9d5f8bfac377
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-14 16:43:53 +0800

    add route

---
+++ resources/js/pages/admin/package/PackageGroupDisplay.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Dashboard',
+        href: route('admin.dashboard'),
+    },
+    {
+        title: 'Package Groups',
+        href: route('admin.packages.groups'),
+    },
+];
+
+</script>
+
+<template>
+    <Head title="Package Groups" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        this is package group page
+    </AppLayout>
+</template>
