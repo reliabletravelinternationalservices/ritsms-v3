6ea6b2070508c8f52a7fe98f7a990b35b65141ac
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-22 15:17:50 +0800

    add request

---
+++ app/Http/Requests/Admin/Package/CreatePackageGroupRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests\Admin\Package;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreatePackageGroupRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth('web')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'include_as_outbound' => 'required|boolean',
+            'include_as_inbound' => 'required|boolean',
+            'is_featured' => 'required|boolean',
+            'image' => 'nullable|image|mimes:jpeg,png,jpg,webp',
+        ];
+    }
+}
