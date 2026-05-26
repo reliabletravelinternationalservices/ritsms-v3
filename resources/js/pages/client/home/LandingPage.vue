5bb3033e8aea04ff338a90a36dee7fb2fa7f9542
Jmgty <joelgutlay04@gmail.com>
2026-05-26 19:50:49 +0800

    apply animation

--- resources/js/pages/client/home/LandingPage.vue
+++ resources/js/pages/client/home/LandingPage.vue
@@ -12,6 +12,7 @@ import PartnerAgency from './section/PartnerAgency.vue';
 import InquiryForm from './section/InquiryForm.vue';
 import ClientFeedback from './section/ClientFeedback.vue';
 import { Destination } from '@/types/destination';
+import MotionWrapper from '@/components/ui/MotionWrapper.vue';
 
 interface PackageResult {
     packages: Package[];
@@ -26,23 +27,51 @@ interface Props {
 
 const page = usePage<Record<string, any>>();
 
-const usdRate: number = page.props.settings?.usd_to_php_rate?? null;
+const usdRate: number = page.props.settings?.usd_to_php_rate ?? null;
 
 const props = defineProps<Props>();
 
 </script>
 
 <template>
+
     <Head title="Discover The World With Us" />
     <AppLayout>
-        <CarouselSection />
-        <DestinationCarousel :destinations="destinations" />
-        <InboundPackageCarousel :packages="props.inbound.packages" tag="VALID FOR FOREIGN ONLY!" :usdRate="usdRate" />
-        <OutboundPackageCarousel :packages="props.outbound.packages" :tag="null" :usdRate="null" />
-        <AgencyServices />
-        <AboutAgency />
-        <PartnerAgency />
-        <InquiryForm />
-        <ClientFeedback />
-    </AppLayout>     
+        <MotionWrapper :delay="0">
+            <CarouselSection />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.05">
+            <DestinationCarousel :destinations="destinations" />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.1">
+            <InboundPackageCarousel :packages="props.inbound.packages" tag="VALID FOR FOREIGN ONLY!"
+                :usdRate="usdRate" />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.15">
+            <OutboundPackageCarousel :packages="props.outbound.packages" :tag="null" :usdRate="null" />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.2">
+            <AgencyServices />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.25">
+            <AboutAgency />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.3">
+            <PartnerAgency />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.35">
+            <InquiryForm />
+        </MotionWrapper>
+
+        <MotionWrapper :delay="0.4">
+            <ClientFeedback />
+        </MotionWrapper>
+    </AppLayout>
 </template>
