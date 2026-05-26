381ed176c343ef781ef5a10c623a63270410fc66
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-26 10:20:34 +0800

    add destination

--- app/Http/Controllers/Admin/Destination/ServiceCountryController.php
+++ app/Http/Controllers/Admin/Destination/ServiceCountryController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Destination;
 
 use App\Http\Controllers\Controller;
+use App\Models\Destination;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
@@ -10,6 +11,19 @@ class ServiceCountryController extends Controller
 {
     public function index()
     {
-        return Inertia::render('admin/destination/ServiceCountry');
+        $destinations = Destination::with(['image', 'locations'])->get();
+
+        return Inertia::render('admin/destination/ServiceCountry', [
+            'destinations' => $destinations,
+        ]);
+    }
+
+    public function show(int $destination)
+    {
+        $destination = Destination::with(['image', 'locations'])->findOrFail($destination);
+
+        return Inertia::render('admin/destination/DestinationDetail', [
+            'destination' => $destination,
+        ]);
     }
 }
