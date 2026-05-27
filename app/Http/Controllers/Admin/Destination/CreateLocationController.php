90f01edfa49aaecb5db5f5da61d98b2f2c838770
Jmgty <joelgutlay04@gmail.com>
2026-05-27 14:48:35 +0800

    update controller add image upload

--- app/Http/Controllers/Admin/Destination/CreateLocationController.php
+++ app/Http/Controllers/Admin/Destination/CreateLocationController.php
@@ -6,22 +6,29 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\Destination\DestinationLocationRequest;
 use App\Repository\Destination\DestinationLocationRepository;
 use App\Repository\Destination\DestinationRepository;
-use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class CreateLocationController extends Controller
 {
-    public function __construct(protected DestinationRepository $repository, protected DestinationLocationRepository $locationRepository){}
+    public function __construct(protected DestinationRepository $repository, protected DestinationLocationRepository $locationRepository) {}
+
     public function index(int $destID)
     {
         $destination = $this->repository->getDestinationByID($destID);
+
         return Inertia::render('admin/destination/CreateLocation', compact('destination'));
     }
 
     public function store(DestinationLocationRequest $request, int $destID)
     {
         $validatedData = $request->validated();
-        $this->locationRepository->createLocation($validatedData, $destID);
-        return redirect()->intended(route('admin.destinations.edit', $destID))->with('success', 'Location created successfully!');
+        $location = $this->locationRepository->createLocation($validatedData, $destID);
+
+        // Handle image upload if provided
+        if ($request->hasFile('image')) {
+            $this->locationRepository->createLocationImage($location, $request->file('image'));
+        }
+
+        return redirect()->route('admin.destinations.details', $destID)->with('success', 'Location created successfully.');
     }
 }
