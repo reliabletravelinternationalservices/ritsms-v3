6d1c4cdd3da27afb831f0019f587afd24af5394c
Jmgty <joelgutlay04@gmail.com>
2026-05-27 14:45:35 +0800

    add function

--- app/Repository/Destination/DestinationLocationRepository.php
+++ app/Repository/Destination/DestinationLocationRepository.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Repository\Destination;
 
 use App\Models\DestinationLocation;
@@ -16,7 +17,7 @@ class DestinationLocationRepository
 
     public function createManyDestinationLocation(array $destinations)
     {
-        return collect($destinations)->map(fn (array $item) => $this->createDestinationLocation($item));
+        return collect($destinations)->map(fn(array $item) => $this->createDestinationLocation($item));
     }
 
 
@@ -24,5 +25,27 @@ class DestinationLocationRepository
     {
         $data['destination_id'] = $destID;
         return $this->createDestinationLocation($data);
+    }
+
+
+
+    public function createLocationImage(DestinationLocation $location, object $image)
+    {
+
+        $folderPath = "upload/location/{$location->id}";
+        $path = $image->store($folderPath, 'public');
+        return DB::transaction(function () use ($location, $image, $path) {
+            return $location->image()->create([
+                'file_name' => $image->getClientOriginalName(),
+                'file_path' => $path,
+                'url' => url('storage/' . $path),
+                'disk' => 'public',
+                'mime_type' => $image->getMimeType(),
+                'size' => $image->getSize(),
+                'alt_text' => $location->name,
+                'order_number' => 1,
+                'is_primary' => true,
+            ]);
+        });
     }
 }
