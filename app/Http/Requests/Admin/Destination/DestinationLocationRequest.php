4728fdaba2642172ac63605fac4c568d74de2f24
Jmgty <joelgutlay04@gmail.com>
2026-05-27 14:59:25 +0800

    add validation request

--- app/Http/Requests/Admin/Destination/DestinationLocationRequest.php
+++ app/Http/Requests/Admin/Destination/DestinationLocationRequest.php
@@ -24,8 +24,9 @@ class DestinationLocationRequest extends FormRequest
     {
         return [
             'name' => ['required', 'string', 'max:255'],
-            'description' => ['nullable', 'string'],
+            'description' => ['required', 'string'],
             'map_link' => ['nullable', 'url'],
+            'image' => ['nullable', 'image', 'mimes:jpeg,png,jpg,webp', 'max:5120'],
         ];
     }
 
@@ -35,8 +36,12 @@ class DestinationLocationRequest extends FormRequest
             'name.required' => 'The location name is required.',
             'name.string' => 'The location name must be a string.',
             'name.max' => 'The location name may not be greater than 255 characters.',
+            'description.required' => 'The location description is required.',
             'description.string' => 'The description must be a string.',
             'map_link.url' => 'The map link must be a valid URL.',
+            'image.image' => 'The image must be a valid image file.',
+            'image.mimes' => 'The image must be a JPEG, PNG, JPG, or WEBP file.',
+            'image.max' => 'The image may not be greater than 5MB.',
         ];
     }
 }
