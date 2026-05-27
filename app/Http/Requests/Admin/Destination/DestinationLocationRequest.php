dfb745212d389a62e20bf2295dd3c5b7e48f2987
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-27 11:57:29 +0800

    add request validation

---
+++ app/Http/Requests/Admin/Destination/DestinationLocationRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests\Admin\Destination;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class DestinationLocationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth('web')->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'map_link' => ['nullable', 'url'],
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'The location name is required.',
+            'name.string' => 'The location name must be a string.',
+            'name.max' => 'The location name may not be greater than 255 characters.',
+            'description.string' => 'The description must be a string.',
+            'map_link.url' => 'The map link must be a valid URL.',
+        ];
+    }
+}
