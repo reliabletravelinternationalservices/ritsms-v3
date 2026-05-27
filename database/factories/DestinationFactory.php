5e40f896916a9a2e3aaf9264baa9d4068e29b468
Jmgty <joelgutlay04@gmail.com>
2026-05-27 15:00:27 +0800

    add factory

---
+++ database/factories/DestinationFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Destination;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Destination>
+ */
+class DestinationFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(3),
+            'description' => fake()->paragraph(),
+            'country' => fake()->country(),
+            'tag' => fake()->word(),
+        ];
+    }
+}
