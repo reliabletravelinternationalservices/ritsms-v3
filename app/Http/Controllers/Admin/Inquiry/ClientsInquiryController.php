88acd41f23acad245f430e6275a65fcbc1438350
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-05-28 14:13:17 +0800

    add repository

--- app/Http/Controllers/Admin/Inquiry/ClientsInquiryController.php
+++ app/Http/Controllers/Admin/Inquiry/ClientsInquiryController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers\Admin\Inquiry;
 
 use App\Http\Controllers\Controller;
+use App\Repository\Inquiry\InquiryRepository;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class ClientsInquiryController extends Controller
 {
+    public function __construct(protected InquiryRepository $repository){}
     public function index()
     {
-        return Inertia::render('admin/inquiry/ClientsInquiry');
+        $inquiries = $this->repository->getAllInquiries();
+        return Inertia::render('admin/inquiry/ClientsInquiry', compact('inquiries'));
     }
 }
