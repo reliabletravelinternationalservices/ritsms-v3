e7868ff4d726ac1c01637c0f4ecb1c27777e5548
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-03 15:32:28 +0800

    add table

---
+++ database/migrations/2026_06_03_072904_create_social_accounts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('social_accounts', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('traveler_id')->constrained('travelers')->cascadeOnDelete();
+            $table->string('provider_name');
+            $table->string('provider_id');
+            $table->timestamps();
+
+            $table->unique(['provider_name', 'provider_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('social_accounts');
+    }
+};
