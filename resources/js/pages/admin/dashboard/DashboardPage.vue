d679abd58e7a616e2ccab20c227b799dedfd211b
Jmgty <joelgutlay04@gmail.com>
2026-06-04 09:38:28 +0800

    change date sorting on  inquiry table

--- resources/js/pages/admin/dashboard/DashboardPage.vue
+++ resources/js/pages/admin/dashboard/DashboardPage.vue
@@ -3,7 +3,8 @@ import SummaryCard from '@/components/statistic/SummaryCard.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import MonthlyInquiriesChart, { type ChartDataPoint } from '@/components/statistic/MonthlyInquiriesChart.vue';
+import MonthlyInquiriesChart from '@/components/statistic/MonthlyInquiriesChart.vue';
+import type { ChartDataPoint } from '@/types/statistics';
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -35,67 +36,44 @@ const props = withDefaults(defineProps<Props>(), {
         inquiries: { count: 0, trend_value: 0, trend_type: 'neutral' },
         collections: { count: 0, trend_value: 0, trend_type: 'neutral' },
         bookings: { count: 0, trend_value: 0, trend_type: 'neutral' },
-        monthly_inquiries: () => []
+        monthly_inquiries: []
     })
 });
 
 </script>
 
 <template>
+
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 relative">
             <div class="grid auto-rows-min gap-4 md:grid-cols-5">
                 <div class="relative">
-                    <SummaryCard
-                        title="Packages"
-                        :value="props.dashboardData.packages.count"
-                        icon="iconoir:package"
+                    <SummaryCard title="Packages" :value="props.dashboardData.packages.count" icon="iconoir:package"
                         :trend-value="`${props.dashboardData.packages.trend_value}%`"
-                        :trend-type="props.dashboardData.packages.trend_type"
-                        :delay="0.05"
-                    />
+                        :trend-type="props.dashboardData.packages.trend_type" :delay="0.05" />
                 </div>
                 <div class="relative">
-                    <SummaryCard 
-                        title="Destinations"
-                        :value="props.dashboardData.destinations.count"
-                        icon="iconoir:maps-arrow"
-                        :trend-value="`${props.dashboardData.destinations.trend_value}%`"
-                        :trend-type="props.dashboardData.destinations.trend_type"
-                        :delay="0.1"
-                    />
+                    <SummaryCard title="Destinations" :value="props.dashboardData.destinations.count"
+                        icon="iconoir:maps-arrow" :trend-value="`${props.dashboardData.destinations.trend_value}%`"
+                        :trend-type="props.dashboardData.destinations.trend_type" :delay="0.1" />
                 </div>
                 <div class="relative">
-                    <SummaryCard 
-                        title="Inquiries"
-                        :value="props.dashboardData.inquiries.count"
-                        icon="iconoir:mail"
+                    <SummaryCard title="Inquiries" :value="props.dashboardData.inquiries.count" icon="iconoir:mail"
                         :trend-value="`${props.dashboardData.inquiries.trend_value}%`"
-                        :trend-type="props.dashboardData.inquiries.trend_type"
-                        :delay="0.15"
-                    />
+                        :trend-type="props.dashboardData.inquiries.trend_type" :delay="0.15" />
                 </div>
                 <div class="relative">
-                    <SummaryCard 
-                        title="Collections"
-                        :value="props.dashboardData.collections.count"
+                    <SummaryCard title="Collections" :value="props.dashboardData.collections.count"
                         icon="heroicons-outline:collection"
                         :trend-value="`${props.dashboardData.collections.trend_value}%`"
-                        :trend-type="props.dashboardData.collections.trend_type"
-                        :delay="0.2"
-                    />
+                        :trend-type="props.dashboardData.collections.trend_type" :delay="0.2" />
                 </div>
                 <div class="relative">
-                    <SummaryCard 
-                        title="Bookings"
-                        :value="props.dashboardData.bookings.count"
-                        icon="iconoir:book"
+                    <SummaryCard title="Bookings" :value="props.dashboardData.bookings.count" icon="iconoir:book"
                         :trend-value="`${props.dashboardData.bookings.trend_value}%`"
-                        :trend-type="props.dashboardData.bookings.trend_type"
-                        :delay="0.25"
-                    />
+                        :trend-type="props.dashboardData.bookings.trend_type" :delay="0.25" />
                 </div>
             </div>
             <div class="relative">
