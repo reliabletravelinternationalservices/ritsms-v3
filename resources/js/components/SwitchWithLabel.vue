847295a4649b8529b2827adabf76e936811c457d
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-06 16:59:17 +0800

    update

--- resources/js/components/SwitchWithLabel.vue
+++ resources/js/components/SwitchWithLabel.vue
@@ -3,18 +3,32 @@ import { Label } from '@/components/ui/label'
 import { Switch } from '@/components/ui/switch'
 
 interface Props {
-    label?: string
-    isChecked: boolean
-    onChange?: (value: boolean) => void
+  label?: string
+  isChecked: boolean
 }
 
 const props = defineProps<Props>()
 
+const emit = defineEmits<{
+  (e: 'update:isChecked', value: boolean): void
+  (e: 'change', value: boolean): void
+}>()
+
+function handleChange(value: boolean) {
+  emit('update:isChecked', value)
+  emit('change', value)
+}
 </script>
 
 <template>
   <div class="flex items-center space-x-2">
-    <Switch id="airplane-mode"  v-model:modelValue="isChecked" @change="props.onChange" />
-    <Label for="airplane-mode">{{ label }}</Label>
+    <Switch
+      id="airplane-mode"
+      :modelValue="props.isChecked"
+      @update:modelValue="handleChange"
+    />
+    <Label for="airplane-mode">
+      {{ label }}
+    </Label>
   </div>
 </template>
