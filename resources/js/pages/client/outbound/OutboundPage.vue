a21892c209d413d788f80e95313e1ca741916d9b
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-13 14:42:54 +0800

    use component head with content

--- resources/js/pages/client/outbound/OutboundPage.vue
+++ resources/js/pages/client/outbound/OutboundPage.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/ClientAppLayout.vue';
-import { Head } from '@inertiajs/vue3';
 import OutboundTitleHeader from './section/OutboundTitleHeader.vue';
 import OutboundPackageDisplay from './section/OutboundPackageDisplay.vue';
 import { PackageGroup } from '@/types/group-package';
+import HeadContent from '@/components/HeadContent.vue';
 
-
+const apiUrl = import.meta.env.VITE_APP_API_URL;
 interface Props {
     'groups': {
         'featured' : PackageGroup[],
@@ -17,7 +17,13 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <Head title="Explore International Destinations With Us" />
+    <HeadContent
+        title="Explore International Destinations With Us"
+        description="Discover our exclusive outbound travel packages, featuring top destinations worldwide. Whether you're seeking adventure, culture, or relaxation, we have the perfect package for you. Explore our featured and normal groups to find your next unforgettable travel experience."
+        :imageUrl="`${apiUrl}/storage/upload/agency/outbound_image.png`"
+        :url="route('client.outbound')"
+    />
+    
     <AppLayout>
         <OutboundTitleHeader  />
         <OutboundPackageDisplay :featuredGroups="props.groups.featured" :normalGroups="props.groups.normal"/>
