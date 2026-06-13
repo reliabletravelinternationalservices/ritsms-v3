4fd5f30c1d6081506cc7374b2b384fa716c0481d
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-13 14:43:20 +0800

    use component head with content

--- resources/js/pages/client/package/group/PackageGroupPage.vue
+++ resources/js/pages/client/package/group/PackageGroupPage.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import AppLayout from '@/layouts/ClientAppLayout.vue';
 import { PackageGroup } from '@/types/group-package';
-import { Head, usePage } from '@inertiajs/vue3';
+import { usePage } from '@inertiajs/vue3';
 import TitleHeader from './section/TitleHeader.vue';
 import PackageList from './section/PackageList.vue';
+import { computed } from 'vue';
+import HeadContent from '@/components/HeadContent.vue';
 
 
 const page = usePage<Record<string, any>>();
@@ -16,10 +18,23 @@ interface Props {
 const props = defineProps<Props>();
 
 const usdRate = page.props.settings?.usd_to_php_rate?? null;
+const url = computed(() => {
+    const routeName = props.isInbound
+        ? 'client.inbound.package.group'
+        : 'client.outbound.package.group';
+
+    return route(routeName, { id: props.group.id });
+});
+
 </script>
 
 <template>
-    <Head :title="props.group.title" />
+    <HeadContent
+        :title="props.group.title"
+        :description="props.group.description?? undefined"
+        :imageUrl="props.group.image && props.group.image.url ? props.group.image.url : undefined"
+        :url="url"
+    />
     <AppLayout>
         <TitleHeader  :title="props.group.title" :description="props.group.description" :image="props.group.image" :isInbound="props.isInbound" :id="props.group.id" />
         <PackageList :packages="props.group.packages?? []" :isInbound="props.isInbound" :usdRate="usdRate" />
