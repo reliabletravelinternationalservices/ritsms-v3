533b7958bc61ef34a79280f52934f7127ea1518a
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-22 16:14:05 +0800

    get all admin user

--- app/Http/Controllers/Admin/User/AdminManagementController.php
+++ app/Http/Controllers/Admin/User/AdminManagementController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\Admin\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Models\User;
 
 class AdminManagementController extends Controller
 {
     public function index()
     {
-        return Inertia::render('admin/user/admin/AdminManagement');
+        $admins = User::all();
+        return Inertia::render('admin/user/admin/AdminManagement', compact('admins'));
     }
 }
