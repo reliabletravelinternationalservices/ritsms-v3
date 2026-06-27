7a068f2278744c7be69295e586547f72b0663507
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-06-27 11:31:41 +0800

    add verification mailer

---
+++ app/Mail/VerifyEmployeeEmail.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Attachment;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class VerifyEmployeeEmail extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+    /**
+     * Create a new message instance.
+     */
+    public string $name;
+    public string $verificationUrl;
+
+    public function __construct(
+        string $name,
+        string $verificationUrl
+    ) {
+        $this->name = $name;
+        $this->verificationUrl = $verificationUrl;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Verify Reliable International Travel Service Account',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'mail.verify',
+            with: [
+                'name' => $this->name,
+                'verificationUrl' => $this->verificationUrl
+            ]
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
