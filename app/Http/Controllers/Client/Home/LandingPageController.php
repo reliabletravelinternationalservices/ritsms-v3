751937f53519a0c334803fdb311a5efbfd9a00ba
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-09 20:11:49 +0800

    use landing page service

--- app/Http/Controllers/Client/Home/LandingPageController.php
+++ app/Http/Controllers/Client/Home/LandingPageController.php
@@ -5,31 +5,18 @@ namespace App\Http\Controllers\Client\Home;
 use App\Http\Controllers\Controller;
 use App\Repository\Destination\DestinationRepository;
 use App\Repository\Package\PackageRepository;
+use App\Services\Client\LandingService;
 use Inertia\Inertia;
 use Illuminate\Support\Facades\View;
 
 class LandingPageController extends Controller
 {
-    public function __construct(protected PackageRepository $packageRepository, protected DestinationRepository $destinationRepository) {}
+    public function __construct(protected LandingService $service) {}
+
 
     public function index()
     {
-
-        $inbound = $this->packageRepository->getFeaturedInboundPackages();
-        $outbound = $this->packageRepository->getFeaturedOutboundPackages();
-        $destinations = $this->destinationRepository->getAllDestinations();
-
-        View::share('seo', [
-            'title' => 'Discover The World With Us',
-            'description' => 'Discover unforgettable travel experiences with Reliable International Travel Services. Explore our featured inbound and outbound packages, and find your perfect destination today!',
-            'image' => asset('storage/upload/agency/logo.png'),
-            'url' => url()->current(),
-        ]);
-
-        return Inertia::render('client/home/LandingPage', [
-            'inbound' => $inbound,
-            'outbound' => $outbound,
-            'destinations' => $destinations
-        ]);
+        $this->service->initializeSEO();
+        return Inertia::render('client/home/LandingPage', $this->service->getData());
     }
 }
