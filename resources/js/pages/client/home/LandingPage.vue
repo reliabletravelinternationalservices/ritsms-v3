da81d3c12fe574896ed7ef72a19ea403ead6280c
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-09 20:11:15 +0800

    add Landing page structured code

--- resources/js/pages/client/home/LandingPage.vue
+++ resources/js/pages/client/home/LandingPage.vue
@@ -1,8 +1,15 @@
 <script setup lang="ts">
-import CarouselSection from '@/pages/client/home/section/ImageCarousel.vue';
+// LAYOUT
 import AppLayout from '@/layouts/ClientAppLayout.vue';
-import { usePage } from '@inertiajs/vue3';
-import type { Package } from '@/types/package';
+
+// PROPS
+import { type LandingPageProps } from './types';
+
+// COMPONENTS
+import MotionWrapper from '@/components/ui/MotionWrapper.vue';
+import CarouselSection from '@/pages/client/home/section/ImageCarousel.vue';
+
+// SECTIONS
 import  DestinationCarousel from './section/DestinationCarousel.vue';
 import InboundPackageCarousel from './section/InboundPackageCarousel.vue';
 import OutboundPackageCarousel from './section/OutboundPackageCarousel.vue';
@@ -11,65 +18,50 @@ import AboutAgency from './section/AboutAgency.vue';
 import PartnerAgency from './section/PartnerAgency.vue';
 import InquiryForm from './section/InquiryForm.vue';
 import ClientFeedback from './section/ClientFeedback.vue';
-import { Destination } from '@/types/destination';
-import MotionWrapper from '@/components/ui/MotionWrapper.vue';
-import HeadContent from '@/components/HeadContent.vue';
 
-interface PackageResult {
-    packages: Package[];
-}
+//logic
+import { useLanding } from './useLanding';
 
-interface Props {
-    destinations: Destination[];
-    inbound: PackageResult;
-    outbound: PackageResult;
-}
+const  { delays, VALID_FOR_FOREIGN_ONLY_TAG } = useLanding();
 
-
-const page = usePage<Record<string, any>>();
-
-const usdRate: number = page.props.settings?.usd_to_php_rate ?? null;
-
-const props = defineProps<Props>();
+defineProps<LandingPageProps>();
 
 </script>
 
 <template>
 
-    <HeadContent title="Discover The World With Us" :url="route('client.landing')" />
     <AppLayout>
         <CarouselSection />
 
-        <MotionWrapper :delay="0.05">
+        <MotionWrapper :delay="delays.destination">
             <DestinationCarousel :destinations="destinations" />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.1">
-            <InboundPackageCarousel :packages="props.inbound.packages" :tag="`VALID FOR FOREIGN ONLY!`"
-                :usdRate="usdRate" />
+        <MotionWrapper :delay="delays.inbound">
+            <InboundPackageCarousel :packages="inbound.packages" :tag="VALID_FOR_FOREIGN_ONLY_TAG" />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.15">
-            <OutboundPackageCarousel :packages="props.outbound.packages" :tag="null" :usdRate="null" />
+        <MotionWrapper :delay="delays.outbound">
+            <OutboundPackageCarousel :packages="outbound.packages" />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.2">
+        <MotionWrapper :delay="delays.services">
             <AgencyServices />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.25">
+        <MotionWrapper :delay="delays.about">
             <AboutAgency />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.3">
+        <MotionWrapper :delay="delays.partner">
             <PartnerAgency />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.35">
+        <MotionWrapper :delay="delays.inquiry">
             <InquiryForm />
         </MotionWrapper>
 
-        <MotionWrapper :delay="0.4">
+        <MotionWrapper :delay="delays.feedback">
             <ClientFeedback />
         </MotionWrapper>
     </AppLayout>
