34a66789c20207a05eadf9b33f41e5b7e0bde94d
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 16:19:26 +0800

    use import  props

--- resources/js/pages/client/location/LocationPage.vue
+++ resources/js/pages/client/location/LocationPage.vue
@@ -1,21 +1,22 @@
 <script setup lang="ts">
+// layouts
 import AppLayout from '@/layouts/ClientAppLayout.vue';
+
+// COMPONENTS
 import TitleHeader from './section/TitleHeader.vue';
-import { Destination } from '@/types/destination';
 import LocationList from './section/LocationList.vue';
-import HeadContent from '@/components/HeadContent.vue';
 
-interface Props {
-    destination: Destination
-}
+// TYPES
+import { DestinationProps } from './types.js';
 
-const props = defineProps<Props>();
+
+defineProps<DestinationProps>();
 
 </script>
 
 <template>
     <AppLayout>
-        <TitleHeader :title="props.destination.title" :description="props.destination.description"  :image="props.destination.image" />
-        <LocationList :locations="props.destination.locations" />
+        <TitleHeader :title="destination.title" :description="destination.description"  :image="destination.image" />
+        <LocationList :locations="destination.locations" />
     </AppLayout>     
 </template>
