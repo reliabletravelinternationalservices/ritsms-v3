9a3764a07adffabf8777f6249396e6a29c155ea8
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 16:22:23 +0800

    add other page SEO And data fetch in 1 class

---
+++ app/Services/Client/DestinationPageService.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Services\Client;
+
+use App\Models\Destination;
+use App\Repository\Destination\DestinationRepository;
+use App\Services\Share\PublicShare;
+
+class DestinationPageService
+{
+    public function __construct(
+        protected DestinationRepository $repository,
+        protected PublicShare $share
+        ) {}
+
+    public function getAllDestinationData()
+    {
+        $destinations = $this->repository->getAllDestinations();
+        return compact('destinations');
+    }
+
+    public function getDestinationByID(int $id)
+    {
+        $destination =$this->repository->getDestinationByID($id);
+        return compact('destination');
+    }
+
+    public function initializeRootPageSEO()
+    {
+        $this->share->SEO(
+            'Explore Most Popular Destinations Around The World',
+            "Discover the world's most popular travel destinations, from stunning beaches to vibrant cities. Start planning your next unforgettable adventure today.",
+            asset('storage/upload/agency/destination_image.png'),
+        );
+    }
+
+
+    public function initializeCountriesPageSEO(string|null $imageUrl = null)
+    {
+        $this->share->SEO(
+            'Countries & Travel Destinations We Serve',
+            'Browse the full list of countries and destinations covered by our travel packages, tours, and ticketing services. We\'re continuously expanding to new destinations worldwide.',
+            asset($imageUrl?? config('assets.logo')),
+        );
+    }
+
+
+    public function initializeDestinationPageSEO(Destination $destination)
+    {
+        $this->share->SEO(
+            $destination->title,
+            $destination->description,
+            asset($destination->image->url?? config('assets.logo')),
+        );
+    }
+
+}
