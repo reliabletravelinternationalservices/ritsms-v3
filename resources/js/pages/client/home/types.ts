66729edef189a4cc1d3507f2ad4b41724934327e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 14:23:38 +0800

    add interface

--- resources/js/pages/client/home/types.ts
+++ resources/js/pages/client/home/types.ts
@@ -16,3 +16,27 @@ export interface CarouselSectionProps {
   packages?: Package[],
   tag?: string | null,
 }
+
+
+export interface ClassProps {
+    class?: string
+}
+
+
+export interface ImageProps {
+  id: number
+  src: string
+  alt?: string
+}
+
+
+export interface DestinationProps {
+    destinations: Destination[]
+}
+
+export interface ServiceProps {
+    id: number,
+    name: string,
+    description: string,
+    icon: string
+}
