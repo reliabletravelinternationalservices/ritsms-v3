1b49d1ad41d1e71f475f23484ab12ff7633df012
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 16:17:13 +0800

    add intrerface

---
+++ resources/js/pages/client/country/types.ts
@@ -0,0 +1,13 @@
+import { Destination } from "@/types/destination";
+import { Media } from "@/types/media";
+
+export interface DestinationProps {
+    destinations: Destination[];
+}
+
+export interface TitleHeaderProps {
+  title: string;
+  description: string;
+  image?: Media;
+}
+
