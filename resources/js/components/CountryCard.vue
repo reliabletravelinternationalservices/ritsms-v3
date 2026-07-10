d8c0784eeeead57fdff58d162111e3a95edc4fe9
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 14:20:27 +0800

    add SEO clarity

--- resources/js/components/CountryCard.vue
+++ resources/js/components/CountryCard.vue
@@ -12,7 +12,7 @@ const props = defineProps<Props>();
 </script>
 
 <template>
-    <Link :href="props.href" class="w-full">
+    <Link :href="props.href" class="w-full" :aria-label="`Explore travel destinations in ${props.destination.country}`">
             <div class="carousel__item h-[400px] md:h-[500px] w-full relative">
               <div class="h-full w-full absolute top-0 left-0 bg-black/40 flex items-end p-6 md:p-8">
                   <span v-if="destination.tag" class="absolute top-2 right-4 text-xs md:text-sm bg-black/30 text-white px-3 py-1">
@@ -20,7 +20,7 @@ const props = defineProps<Props>();
                   </span>
                   <div class="border-l-4 border-[var(--tertiary-custom)] pl-2 text-left">
                       <span class="flex items-end justify-start">
-                          <Icon icon="mdi:map-marker" width="22" height="22" class="text-[var(--tertiary-custom)] md:w-[26px] md:h-[26px]" />
+                          <Icon icon="mdi:map-marker" width="22" height="22" class="text-[var(--tertiary-custom)] md:w-[26px] md:h-[26px]" aria-hidden="true" />
                           <h3 class="font-bold font-montserrat text-base md:text-lg text-[var(--tertiary-custom)]">{{ destination.country }}</h3>
                       </span>
                       <span v-if="props.destination.locations" class="flex items-end justify-start">
@@ -31,14 +31,15 @@ const props = defineProps<Props>();
             <img 
               v-if="destination.image?.url"
               :src="destination.image.url" 
-              :alt="destination.image.alt_text"
+              :alt="destination.image.alt_text || `${destination.country} travel destination`"
+              loading="lazy"
               class="w-full h-full object-cover"
             />
             <div 
               v-else
               class="w-full h-full bg-gray-200 flex items-center justify-center"
             >
-              <Icon icon="mdi:image-off" width="48" height="48" class="text-gray-400" />
+              <Icon icon="mdi:image-off" width="48" height="48" class="text-gray-400" aria-hidden="true" />
             </div>
           </div>
     </Link>
