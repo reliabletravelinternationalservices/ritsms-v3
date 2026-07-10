1a88295892b578ef1ce930ac09ae1c365da11e36
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-10 16:15:54 +0800

    merge functions to 1 file

--- app/Http/Controllers/Client/Destination/DestinationPageController.php
+++ app/Http/Controllers/Client/Destination/DestinationPageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Client\Destination;
 
 use App\Http\Controllers\Controller;
+use App\Models\Destination;
 use App\Services\Client\DestinationPageService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
@@ -12,7 +13,25 @@ class DestinationPageController extends Controller
     public function __construct(private DestinationPageService $service) {}
     public function index()
     {
-        $this->service->initializeSEO();
-        return Inertia::render('client/destination/DestinationPage', $this->service->getData());
+        $this->service->initializeRootPageSEO();
+        return Inertia::render('client/destination/DestinationPage', $this->service->getAllDestinationData());
+    }
+
+
+
+    public function countries()
+    {
+        $data = $this->service->getAllDestinationData();
+        $this->service->initializeCountriesPageSEO($data['destinations'][0]->image->url);
+        return Inertia::render('client/country/ServiceCountryPage', $data);
+    }
+
+
+    public function country(Destination $destination)
+    {
+        $destination = $this->service->getDestinationByID($destination->id);
+        $this->service->initializeDestinationPageSEO($destination['destination']);
+        
+        return Inertia::render('client/location/LocationPage', $destination);
     }
 }
