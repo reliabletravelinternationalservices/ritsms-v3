c8730039253cf03c52caf382a608cfcb42a5c2d1
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-13 14:34:38 +0800

    add page data

--- app/Services/Client/OutboundPageService.php
+++ app/Services/Client/OutboundPageService.php
@@ -2,6 +2,9 @@
 
 namespace App\Services\Client;
 
+use App\Models\Package;
+use App\Models\PackageGroup;
+use App\Repository\Destination\DestinationRepository;
 use App\Repository\Package\PackageGroupRepository;
 use App\Services\Share\PublicShare;
 
@@ -9,27 +12,49 @@ class OutboundPageService
 {
     public function __construct(
         protected PublicShare $share,
-        protected PackageGroupRepository $repository
+        protected PackageGroupRepository $repository,
+        protected DestinationRepository $destRepo
     ) {}
+
+
+    public function getGroupDetailByID(int $id) {
+        $group = $this->repository->getPackageGroupByID($id);
+        $group->load(['image', 'packages.primaryImage']);
+        return $group;
+    }
 
     public function getOutboundPageData()
     {
+        $destinationCountries = $this->destRepo->getDestinctCountries();
         $featuredGroupPackages = $this->repository->getPackageGroupByType('outbound', true);
         $groupPackages = $this->repository->getPackageGroupByType('outbound', false);
 
-        $groups = [
-            'featured' => $featuredGroupPackages->toArray(),
-            'normal' => $groupPackages->toArray(),
-        ];
+        $countries = $destinationCountries->toArray();
+        $featured = $featuredGroupPackages->toArray();
+        $regular = $groupPackages->toArray();
+        
+        return compact('featured', 'regular', 'countries');
+    }
 
-        return compact('groups');
+
+    public function geGroupPackagePageData(PackageGroup $group)
+    {
+        $isInbound = false;
+        return compact('group', 'isInbound');
     }
 
     public function initializeRootPageSEO() {
         $this->share->SEO(
             'Outbound Travel Packages & International Destinations',
             'Discover exclusive outbound travel packages to top international destinations. Find adventure, culture, or relaxation with our trusted Philippine travel agency.',
-            asset('storage/upload/agency/outbound_image.png'),
+            asset('storage/upload/agency/outbound_image.png')
+        );
+    }
+    public function initializeGroupDetailPageSEO(PackageGroup $group) {
+        $this->share->SEO(
+            $group->title,
+            $group->description,
+            asset($group->primaryImage->url?? config('assets.logo')),
         );
     }
 }
