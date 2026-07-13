a1c3be2ae17364b7f7f253b8b50af4d4a180956e
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-13 17:27:33 +0800

    use type

--- resources/js/pages/client/inbound/InboundPage.vue
+++ resources/js/pages/client/inbound/InboundPage.vue
@@ -2,32 +2,17 @@
 import AppLayout from '@/layouts/ClientAppLayout.vue';
 import InboundTitleHeader from './section/InboundTitleHeader.vue';
 import InboundPackageDisplay from './section/InboundPackageDisplay.vue';
-import { PackageGroup } from '@/types/group-package';
-import HeadContent from '@/components/HeadContent.vue';
+import { InboundPageProps } from '../outbound/types.js';
 
-const appUrl = import.meta.env.VITE_APP_URL;
-interface Props {
-    'groups': {
-        'featured': PackageGroup[],
-        'normal': PackageGroup[],
-    };
-    destinationLocations: string[];
-}
 
-const props = defineProps<Props>();
+defineProps<InboundPageProps>();
 </script>
 
 <template>
 
-    <HeadContent
-        title="Explore Local Spots Here in The Philippines"
-        description="Discover the best local spots in the Philippines with our comprehensive guide. From stunning beaches to vibrant cities, explore hidden gems and popular destinations across the country. Plan your next adventure with us and experience the rich culture, breathtaking landscapes, and warm hospitality of the Philippines."
-        :imageUrl="`${appUrl}/storage/upload/agency/inbound_image.png`"
-        :url="route('client.inbound')"
-    />
     <AppLayout>
         <InboundTitleHeader />
-        <InboundPackageDisplay :featuredGroups="props.groups.featured" :normalGroups="props.groups.normal"
-            :destinationLocations="props.destinationLocations" />
+        <InboundPackageDisplay :featuredGroups="featured" :normalGroups="regular"
+            :destinationLocations="locations" />
     </AppLayout>
 </template>
