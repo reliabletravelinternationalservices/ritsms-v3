8a3af97b93236263336578205ff1e2b7fff5e481
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-13 16:04:48 +0800

    arrange page service

--- app/Services/Client/OutboundPageService.php
+++ app/Services/Client/OutboundPageService.php
@@ -6,6 +6,7 @@ use App\Models\Package;
 use App\Models\PackageGroup;
 use App\Repository\Destination\DestinationRepository;
 use App\Repository\Package\PackageGroupRepository;
+use App\Repository\Package\PackageRepository;
 use App\Services\Share\PublicShare;
 
 class OutboundPageService
@@ -13,16 +14,12 @@ class OutboundPageService
     public function __construct(
         protected PublicShare $share,
         protected PackageGroupRepository $repository,
-        protected DestinationRepository $destRepo
+        protected DestinationRepository $destRepo,
+        protected PackageRepository $packageRepo
     ) {}
 
 
-    public function getGroupDetailByID(int $id) {
-        $group = $this->repository->getPackageGroupByID($id);
-        $group->load(['image', 'packages.primaryImage']);
-        return $group;
-    }
-
+    // ROOT PAGE
     public function getOutboundPageData()
     {
         $destinationCountries = $this->destRepo->getDestinctCountries();
@@ -37,12 +34,6 @@ class OutboundPageService
     }
 
 
-    public function geGroupPackagePageData(PackageGroup $group)
-    {
-        $isInbound = false;
-        return compact('group', 'isInbound');
-    }
-
     public function initializeRootPageSEO() {
         $this->share->SEO(
             'Outbound Travel Packages & International Destinations',
@@ -50,6 +41,20 @@ class OutboundPageService
             asset('storage/upload/agency/outbound_image.png')
         );
     }
+
+
+
+    // PACKAGE GROUP DETAILS PAGE
+    public function getGroupDetailByID(int $id) {
+        return $this->repository->getPackageGroupByID($id); 
+    }
+
+    public function geGroupPackagePageData(PackageGroup $group)
+    {
+        $isInbound = false;
+        return compact('group', 'isInbound');
+    }
+
     public function initializeGroupDetailPageSEO(PackageGroup $group) {
         $this->share->SEO(
             $group->title,
@@ -57,4 +62,30 @@ class OutboundPageService
             asset($group->primaryImage->url?? config('assets.logo')),
         );
     }
+
+
+
+
+
+    // PACKAGE DETAILS PAGE
+    public function getPackageBySlug(string $slug) {
+        return $this->packageRepo->getPackageBySlug($slug);
+        
+    }
+
+    public function getPackageDetailPageData(Package $package) {
+        $isInbound = false;
+        return compact('package', 'isInbound');
+    }
+
+
+    public function initializePackageDetailPageSEO(Package $package) {
+        $this->share->SEO(
+            $package->name,
+            $package->description,
+            asset($package->primaryImage->url?? config('assets.logo')),
+        );
+    }
+
+
 }
