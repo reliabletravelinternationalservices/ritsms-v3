dabb4feacb7c5a1b57f09a4783957d75d764e3c5
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-13 14:33:11 +0800

    add test

--- tests/Feature/Controllers/Client/OutboundPageControllerTest.php
+++ tests/Feature/Controllers/Client/OutboundPageControllerTest.php
@@ -4,4 +4,8 @@ test('outbound page renders successfully', function () {
     $response = $this->get(route('client.outbound'));
 
     $response->assertStatus(200);
+    $response->assertInertia(fn ($page) => $page
+        ->component('client/outbound/OutboundPage')
+        ->has('countries')
+    );
 });
