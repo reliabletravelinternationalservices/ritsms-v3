e6226c974cfd9cc0d2c2c35ddc67e90a25935318
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-15 10:11:31 +0800

    add page index

--- app/Http/Controllers/Client/InboundPageController.php
+++ app/Http/Controllers/Client/InboundPageController.php
@@ -19,4 +19,21 @@ class InboundPageController extends Controller
         $this->service->initializeRootPageSEO();
         return Inertia::render('client/inbound/InboundPage', $this->service->getInboundPageData());
     }
+
+
+
+    public function groupDetail(int $id)
+    {
+        $group = $this->service->getGroupDetailByID($id);
+        $this->service->initializeGroupDetailPageSEO($group);
+        return Inertia::render('client/package/group/PackageGroupPage', $this->service->geGroupPackagePageData($group));
+    }
+
+
+    public function packageDetail(string $slug)
+    {
+        $package = $this->service->getPackageBySlug($slug);
+        $this->service->initializePackageDetailPageSEO($package);
+        return Inertia::render('client/package/detail/PackageDetailPage', $this->service->getPackageDetailPageData($package));
+    }
 }
