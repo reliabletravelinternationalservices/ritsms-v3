2529c331e027fea88d833362bab66be07766d166
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-23 11:42:06 +0800

    use country with location

--- resources/js/services/countryService.ts
+++ resources/js/services/countryService.ts
@@ -1,11 +1,18 @@
 import api from '@/lib/api'
 import type { ApiResponse } from '@/types/api'
-import type { Country } from '@/types/country'
+import type { CountryFilter, CountryWithLocations } from '@/types/country'
 
 export const countryService = {
-    async getAll(): Promise<ApiResponse<Country[]>> {
-        const { data } = await api.get<ApiResponse<Country[]>>('/countries')
-
+    async getAll(filters: CountryFilter = {}): Promise<ApiResponse<CountryWithLocations[]>> {
+        const { data } = await api.get<ApiResponse<CountryWithLocations[]>>('/countries', {
+            params: filters
+        })
         return data
     },
+
+
 }
+
+
+
+
