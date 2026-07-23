859acebab4911e8c5ca8371c8583bd06bc6d9018
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-23 11:42:23 +0800

    add types

--- resources/js/types/country.ts
+++ resources/js/types/country.ts
@@ -10,3 +10,27 @@ export interface Country {
     locations_count: number
     image: Image | null
 }
+
+
+
+export type CountryWithLocations = Country & {
+    locations: Location[]
+}
+
+
+
+export interface Location {
+    id: number
+    name: string
+    image: Image | null
+    description: string
+}
+
+
+export interface CountryFilter {
+    countryName?: string
+    with?: {
+        locations?: boolean
+    }
+    perPage?: number
+}
