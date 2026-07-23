3c8552b58740eb25c9367ba24b7311d8fcdfaf72
reliabletravelinternationalservices <reliabletravelinfo@gmail.com>
2026-07-23 11:40:21 +0800

    add pagination

--- resources/js/composables/services/useCountries.ts
+++ resources/js/composables/services/useCountries.ts
@@ -1,21 +1,23 @@
 import { ref } from 'vue'
 import { countryService } from '@/services/countryService'
-import type { Country } from '@/types/country'
+import type { Country, CountryFilter, CountryWithLocations } from '@/types/country'
+import { PaginatedData } from '@/types/api'
 
-export function useCountries() {
-    const countries = ref<Country[]>([])
+export function useCountry() {
+    const paginatedCountries = ref<PaginatedData<CountryWithLocations[]>>()
+    const countries = ref<CountryWithLocations[]>([])
     const loading = ref(false)
     const loaded = ref(false)
     const error = ref<string | null>(null)
 
-    const fetchCountries = async () => {
+    const fetchCountries = async (filters: CountryFilter = {}) => {
         loading.value = true
         error.value = null
 
         try {
-            const response = await countryService.getAll()
-
-            countries.value = response.data
+            const response = await countryService.getAll(filters)
+            paginatedCountries.value = response.data
+            countries.value = response.data.data
             loaded.value = true
         } catch (err: any) {
             error.value =
@@ -29,7 +31,7 @@ export function useCountries() {
         }
     }
 
-    const refresh = () => fetchCountries()
+    const refresh = (filters: CountryFilter = {}) => fetchCountries(filters)
 
     const reset = () => {
         countries.value = []
