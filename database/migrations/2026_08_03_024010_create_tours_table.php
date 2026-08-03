afc22407ce71e72a15fee31dcc61fc58b4ffb98e
Jmgty <joelgutlay04@gmail.com>
2026-08-03 13:00:50 +0800

    add table

---
+++ database/migrations/2026_08_03_024010_create_tours_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->id();
+            $table->string('code', 20)->unique();
+            $table->string('slug', 100)->unique();
+            $table->string('name', 120);
+            $table->enum('category', ['domestic', 'inbound', 'outbound']);
+            $table->enum('itinerary_type', ['round_trip', 'multi_city', 'one_way']);
+            $table->enum('tour_type', ['regular', 'private', 'custom', 'group']);
+            $table->enum('state', ['draft', 'published', 'archived'])->default('draft');
+            $table->enum('visibility', ['public', 'private'])->default('private');
+            $table->unsignedInteger('duration')->default(1);
+            $table->json('highlights');
+            $table->json('inclusions');
+            $table->json('exclusions')->nullable();
+            $table->json('terms_and_conditions');
+            $table->json('description');
+            $table->string('badge', 100)->nullable();
+            $table->text('notes')->nullable()
+                ->comment('For Internal Use Only - Not visible to customers');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tours');
+    }
+};
