40a818966b8a2fab70fc02780fd7dfac0d10bbb6
Jmgty <joelgutlay04@gmail.com>
2026-08-05 14:54:22 +0800

    add  user active

--- app/Models/User.php
+++ app/Models/User.php
@@ -57,6 +57,7 @@ class User extends Authenticatable implements MustVerifyEmail
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+            'is_active' => 'boolean',
         ];
     }
 
