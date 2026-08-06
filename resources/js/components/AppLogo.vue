526f01c9d58cd074c955c8c365b8d4c0012016a9
Jmgty <joelgutlay04@gmail.com>
2026-08-06 10:02:50 +0800

    update logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -9,10 +9,10 @@ defineProps<Props>();
 </script>
 
 <template>
-    <div class="flex aspect-square size-8 items-center justify-center rounded-none bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-7" />
+    <div class="flex aspect-square size-8 items-center justify-center rounded-none">
+        <AppLogoIcon class=" size-8 fill-current" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
-        <span class="mb-0.5 truncate font-semibold leading-none">RITS ADMIN PANEL</span>
+        <span class="mb-0.5 truncate font-semibold leading-none">ADMIN PANEL</span>
     </div>
 </template>
