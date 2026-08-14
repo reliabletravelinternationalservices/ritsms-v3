f2759c127f9741d4c2e744f0a7c3f3380408a808
Jmgty <joelgutlay04@gmail.com>
2026-08-14 20:50:13 +0800

    add reusable form

---
+++ resources/js/components/form/tour/TourForm.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import { useTourFormStore } from '@/stores/tourForm'
+import NavButton from './NavButton.vue';
+import OverviewForm from './OverviewSection.vue';
+
+const tourForm = useTourFormStore()
+
+const { isCurrentSection, setSection, SECTION } = tourForm
+</script>
+
+<template>
+    <div class="flex gap-4">
+        <NavButton v-for="section in tourForm.sections" :key="section.key" :label="section.label"
+            :active="tourForm.currentSection === section.key" @click="setSection(section.key)" />
+
+    </div>
+    <div class="mt-4 p-4 text-foreground">
+        <div v-if="isCurrentSection(SECTION.OVERVIEW)">
+            <OverviewForm />
+        </div>
+        <div v-else-if="isCurrentSection(SECTION.ITINERARIES)">
+            this is itinerary form
+        </div>
+        <div v-else-if="isCurrentSection(SECTION.INCLUSIONS)">
+            this is inclusions form
+        </div>
+        <div v-else-if="isCurrentSection(SECTION.EXCLUSIONS)">
+            this is <em></em> form
+        </div>
+        <div v-else-if="isCurrentSection(SECTION.PRICING)">
+            this is pricing form
+        </div>
+    </div>
+</template>
