8870834a1d1c48cf37226a0e478d91f75446933d
Jmgty <joelgutlay04@gmail.com>
2026-08-17 09:39:30 +0800

    update overview section

---
+++ resources/js/components/form/tour/OverviewSection.vue
@@ -0,0 +1,161 @@
+<script setup lang="ts">
+// import InputError from '@/components/InputError.vue';
+import SelectMenu, { SelectOption } from '@/components/SelectMenu.vue';
+import TiptapTextArea from '@/components/tiptap/TextArea.vue';
+import { Input } from '@/components/ui/input';
+import Textarea from '@/components/ui/textarea/Textarea.vue';
+import { useTourFormStore } from '@/stores/tourForm';
+
+
+
+const tourForm = useTourFormStore();
+
+
+const categories: SelectOption[] = [
+    {
+        label: 'Outbound Tour',
+        value: 'outbound',
+    },
+    {
+        label: 'Inbound Tour',
+        value: 'inbound',
+    },
+    {
+        label: 'Domestic Tour',
+        value: 'domestic',
+    }
+]
+
+const daysDurations: SelectOption[] = Array.from({ length: 30 }, (_, index) => {
+    const day = index + 1;
+    return {
+        label: `${day} Day${day === 1 ? '' : 's'}`,
+        value: day.toString(),
+    };
+})
+
+
+const itineraryTypes: SelectOption[] = [
+    {
+        label: 'Round Trip',
+        value: 'round_trip',
+    },
+    {
+        label: 'One Way',
+        value: 'one_way',
+    },
+    {
+        label: 'Multi City',
+        value: 'multi_city',
+    }
+]
+
+
+const handleDurationChange = (value: string) => {
+    tourForm.syncItineraries(value);
+}
+
+</script>
+
+<template>
+    <div>
+        <!-- TOUR OVERVIEW -->
+        <div>
+            <div class="uppercase text-md font-bold border-b-2 border-foreground w-full py-2">
+                <span>Tour Overview</span>
+            </div>
+            <div class="flex flex-col gap-4 p-4">
+                <div class="space-y-2">
+                    <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Tour name <span
+                            class="text-red-600">*</span></label>
+                    <Input v-model="tourForm.form.overviewItems.name" name="name" placeholder="Enter tour name"
+                        class="font-roboto text-sm" />
+                    <!-- <InputError message="Tour name is required" /> -->
+                </div>
+
+                <div class="flex items-center gap-4">
+                    <div class="space-y-2 w-full">
+                        <label for="category" class="block text-sm font-medium leading-6 text-gray-900">Category <span
+                                class="text-red-600">*</span></label>
+                        <SelectMenu v-model="tourForm.form.overviewItems.category" name="category"
+                            placeholder="Select category" :options="categories" class="font-roboto text-sm" />
+                        <!-- <InputError message="Tour name is required" /> -->
+                    </div>
+                    <div class="space-y-2 w-full">
+                        <label for="duration" class="block text-sm font-medium leading-6 text-gray-900">Duration <span
+                                class="text-xs text-muted-foreground">(days)</span> <span
+                                class="text-red-600">*</span></label>
+                        <SelectMenu v-model="tourForm.form.overviewItems.duration" name="duration"
+                            placeholder="Select duration" :options="daysDurations" class="font-roboto text-sm"
+                            @change="handleDurationChange" />
+                        <!-- <InputError message="Tour name is required" /> -->
+                    </div>
+                </div>
+
+                <div class="flex items-center gap-4">
+                    <div class="space-y-2 w-full">
+                        <label for="itinerary_type" class="block text-sm font-medium leading-6 text-gray-900">Itinerary
+                            Type
+                            <span class="text-red-600">*</span></label>
+                        <SelectMenu v-model="tourForm.form.overviewItems.itinerary_type" name="itinerary_type"
+                            placeholder="Select type" :options="itineraryTypes" class="font-roboto text-sm" />
+                        <!-- <InputError message="Tour name is required" /> -->
+                    </div>
+                    <div class="space-y-2 w-full">
+                        <label for="badge" class="block text-sm font-medium leading-6 text-gray-900">Badge <span
+                                class="text-xs text-muted-foreground">(optional)</span></label>
+                        <Input v-model="tourForm.form.overviewItems.badge" name="badge"
+                            placeholder="e.g. Best Seller, Trending, etc." class="font-roboto text-sm" />
+                        <!-- <InputError message="Tour name is required" /> -->
+                    </div>
+                </div>
+
+                <div class="space-y-2">
+                    <label for="description" class="block text-sm font-medium leading-6 text-gray-900">Description <span
+                            class="text-red-600">*</span></label>
+                    <Textarea v-model="tourForm.form.overviewItems.description" placeholder="Tell us about this tour..."
+                        name="description" />
+                </div>
+
+                <div class="space-y-2">
+                    <label for="highlights" class="block text-sm font-medium leading-6 text-gray-900">Highlights <span
+                            class="text-red-600">*</span></label>
+                    <TiptapTextArea v-model="tourForm.form.overviewItems.highlights"
+                        placeholder="What are the featured destinations in this tour?" name="highlights" />
+                </div>
+            </div>
+        </div>
+
+        <!-- POLICIES -->
+        <div>
+            <div class="uppercase text-md font-bold border-b-2 border-foreground w-full py-2">
+                <span>INCLUSIONS & POLICIES</span>
+            </div>
+            <div class="flex flex-col gap-4 p-4">
+                <div class="flex items-center gap-4">
+                    <div class="space-y-2 w-full">
+                        <label for="inclusions" class="block text-sm font-medium leading-6 text-gray-900">Inclusions
+                            <span class="text-red-600">*</span></label>
+                        <TiptapTextArea v-model="tourForm.form.overviewItems.inclusions"
+                            placeholder="What's included to this tour?" name="inclusions" class="font-roboto text-sm" />
+                        <!-- <InputError message="Tour name is required" /> -->
+                    </div>
+                    <div class="space-y-2 w-full">
+                        <label for="exclusions" class="block text-sm font-medium leading-6 text-gray-900">Exclusions
+                            <span class="text-xs text-muted-foreground">(optional)</span></label>
+                        <TiptapTextArea v-model="tourForm.form.overviewItems.exclusions"
+                            placeholder="What's not included to this tour?" name="exclusions"
+                            class="font-roboto text-sm" />
+                    </div>
+                </div>
+
+                <div class="space-y-2 w-full">
+                    <label for="terms" class="block text-sm font-medium leading-6 text-gray-900">Terms of Services
+                        <span class="text-red-600">*</span></label>
+                    <TiptapTextArea v-model="tourForm.form.overviewItems.terms_and_conditions"
+                        placeholder="What's not included to this tour?" name="terms" class="font-roboto text-sm" />
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
