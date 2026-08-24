e3f9d63c92b7342f20594e58629c0cb79c05b5e0
reliable jo <reliabletravelinfo@gmail.com>
2026-08-24 15:51:54 +0800

    add store

--- app/Http/Controllers/Admin/Tour/CreateTourController.php
+++ app/Http/Controllers/Admin/Tour/CreateTourController.php
@@ -3,14 +3,28 @@
 namespace App\Http\Controllers\Admin\Tour;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Tour\TourRequest;
+use App\Models\Tour;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
 
 class CreateTourController extends Controller
 {
-    public function index(): Response
+    public function create(): Response
     {
         return Inertia::render('admin/tour/CreateTour');
+    }
+
+
+    
+    public function store(TourRequest $request): RedirectResponse
+    {
+        $validatedData = $request->validated();
+
+        $tour = Tour::create($validatedData['overview']);
+
+        return to_route('admin.tours.edit', ['slug' => $tour->slug])->with('success', 'Tour saved successfully.');
     }
 }
