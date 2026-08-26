df18f2ae729df1c7fafd24071fbccfcfa1a0a8bb
reliable jo <reliabletravelinfo@gmail.com>
2026-08-26 11:27:53 +0800

    add global alert diablog

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import GlobalAlertDialog from '@/components/GlobalAlertDialog.vue';
 import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
 import type { BreadcrumbItemType } from '@/types';
 
@@ -15,4 +16,5 @@ withDefaults(defineProps<Props>(), {
     <AppLayout :breadcrumbs="breadcrumbs">
         <slot />
     </AppLayout>
+    <GlobalAlertDialog/>
 </template>
